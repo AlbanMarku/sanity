94dee662fcd0460f079e852271c324f91c022c43
AlbanMarku <albanmarku@outlook.com>
2023-07-31 22:38:02 +0100

    Named object array.

--- schemas/objects/AttackInput.js
+++ schemas/objects/AttackInput.js
@@ -1,9 +1,22 @@
 export default {
-  title: 'Attack Input',
   name: 'attackInput',
   type: 'object',
+  title: 'Attack Input',
   fields: [
     {name: 'direction', type: 'number', title: 'Direction'},
     {name: 'button', type: 'number', title: 'Button'},
   ],
+  preview: {
+    select: {
+      direction: 'direction',
+      button: 'button',
+    },
+    prepare(selection) {
+      const {direction, button} = selection
+      // Format the display of the attackInput object in the Sanity Studio
+      return {
+        title: `Direction: ${direction || 'N/A'}, Button: ${button || 'N/A'}`,
+      }
+    },
+  },
 }
